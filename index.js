c37062408084c863faf9b6c8c4a18d5cf2a63b4d
Chenthill Palanisamy <chenthill.palanisamy@softwareag.com>
2018-07-10 15:22:50 +0530

    Add sequelize, body-parser, logging capabilities.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/hello', (req, res) => res.send (JSON.stringify({'chen': 'Hello guys'})))
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
